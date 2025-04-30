5906080e942818fb150c4dbba892576551a1db28
eliaszlsp <estudoselias@hotmail.com>
2025-04-29 23:25:55 -0300

    update

--- src/bancoDeDados/conexão.js
+++ src/bancoDeDados/conexão.js
@@ -12,11 +12,5 @@ const db = knex({
     port: parseInt(process.env.DB_PORT),
   },
 });
-console.log('\nVariáveis de ambiente atuais:');
-console.log('DB_USER:', process.env.DB_USER || 'não definido');
-console.log('DB_HOST:', process.env.DB_HOST || 'não definido');
-console.log('DB_DATABASE:', process.env.DB_DATABASE || 'não definido');
-console.log('DB_PORT:', process.env.DB_PORT || 'não definido');
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '******' : 'não definido');
 
 module.exports = db;
--- src/controladores/alunos.js
+++ src/controladores/alunos.js
@@ -14,6 +14,7 @@ const listarAlunos = async (req, res) => {
     const alunos = await db("alunos").select();
     return res.status(200).json(alunos);
   } catch (error) {
+    console.log(error)
     res.status(500).json({ error: "Erro ao buscar alunos" });
   }
 };
@@ -27,6 +28,7 @@ const buscarAlunoPorId = async (req, res) => {
       return res.status(404).json({ error: "Aluno não encontrado" });
     }
   } catch (error) {
+
     res.status(500).json({ error: "Erro ao buscar aluno" });
   }
 };
